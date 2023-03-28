9d1df378c44afa89bb29b2f5b3a8e221913440cb
João Toucci <123783909+Toucci@users.noreply.github.com>
2023-03-28 11:24:30 -0300

    Update ContaCorrente.py

--- ContaCorrente.py
+++ ContaCorrente.py
@@ -1,22 +1,32 @@
+"""
+Importando as bibliotecas utilizadas.
+Também importamos a classe Conta, que foi criada em outra aba .py.
+Como a classe Conta_Corrente é filha das características da Conta, importamos ela.
+"""
 from Conta import Conta
 from abc import ABC, abstractmethod
 
+#Criando a classe Conta_Corrente(filha da classe Conta)
 class Conta_Corrente(Conta):
-    def __init__(self, id_conta, saldo, limite):
-        super().__init__(id_conta,saldo)
-        self.__limite = limite
+    def __init__(self, id_conta, saldo, limite): #definindo os atributos na construtora que serão utilizados no decorrer desta classe.
+        super().__init__(id_conta,saldo)    #Serve para chamar o id_conta e saldo da classe Conta.
+        self.__limite = limite      #Privando o atributo limite.
 
+#Criando um decorador para chamar o valor disponível para saque, ao invés de criar no próprio método.
     @property
     def valor_disponivel_para_saque(self):
         return self.saldo + self.__limite
 
+#Método privado criado com a intenção de verificar se o valor escolhido para saque é menor que o valor disponível para saque. Se sim, retornará True.
     def __verifica_limite(self, valor_escolhido_para_sacar) -> bool:
         return valor_escolhido_para_sacar <= self.valor_disponivel_para_saque
 
+#Método privado criado com a intenção de retornar um erro quando o método acima der False(false se o valor escolhido para saque for maior que o disponível) 
     def __retorna_erro_se_passar_limite(self, valor_escolhido_para_sacar):
         if not self.__verifica_limite(valor_escolhido_para_sacar):
             raise ValueError("Quantidade excede o limite para saque!")
-
+            
+#Método privado criado para retornar o valor do saldo se ele for menor do que 0. A intenção é criar uma mensagem quando o saldo entrar no negativo. 
     def __verifica_se_valor_negativo(self) -> bool:
         return self.saldo < 0
 
